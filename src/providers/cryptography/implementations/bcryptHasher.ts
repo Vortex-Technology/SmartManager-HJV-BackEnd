45e4715b26f874fadd0bad3f2f3d13b4180bfee4
João Paulo do Vale <100259992+JPDovale@users.noreply.github.com>
2023-12-16 20:21:48 -0300

    [refactor] remove idiotic handle hash

--- src/providers/cryptography/implementations/bcryptHasher.ts
+++ src/providers/cryptography/implementations/bcryptHasher.ts
@@ -4,12 +4,10 @@ import { HashGenerator } from '../contracts/hashGenerator'
 
 export class BcryptHasher implements HashGenerator, HashComparer {
   async hash(plain: string): Promise<string> {
-    // take one random number between 8 and 32
-    const handleHashSalt = Math.floor(Math.random() * (32 - 8 + 1)) + 8
-    const handleHashSaltWithTowChar = String(handleHashSalt).padStart(2, '0')
+    const hashSalt = 10
 
-    const plainHashed = await hash(plain, handleHashSalt)
-    const encryptedPlainFinal = `${plainHashed}${handleHashSaltWithTowChar}`
+    const plainHashed = await hash(plain, hashSalt)
+    const encryptedPlainFinal = `${plainHashed}${hashSalt}`
 
     return encryptedPlainFinal
   }
