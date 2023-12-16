97ad51892842072ac1c472d960f8112e69a8aaa0
João Paulo do Vale <100259992+JPDovale@users.noreply.github.com>
2023-12-16 20:23:09 -0300

    [add] im memory repository to tests

---
+++ test/repositories/modules/administrator/AdministratorInMemoryRepository.ts
@@ -0,0 +1,32 @@
+import { Administrator } from '@modules/administrator/entities/Administrator'
+import { AdministratorRepository } from '@modules/administrator/repositories/AdministratorRepository'
+
+export class AdministratorInMemoryRepository
+  implements AdministratorRepository
+{
+  administrators: Administrator[] = []
+
+  async create(administrator: Administrator): Promise<void> {
+    this.administrators.push(administrator)
+  }
+
+  async findByLogin(login: string): Promise<Administrator | null> {
+    const administrator = this.administrators.find(
+      (administrator) => administrator.login === login,
+    )
+
+    if (!administrator) return null
+
+    return administrator
+  }
+
+  async findById(id: string): Promise<Administrator | null> {
+    const administrator = this.administrators.find(
+      (administrator) => administrator.id.toString() === id,
+    )
+
+    if (!administrator) return null
+
+    return administrator
+  }
+}
