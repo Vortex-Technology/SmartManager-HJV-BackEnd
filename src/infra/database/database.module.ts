c9be60e9f974a191c1cd75c156bbec84b7fd25a1
João Paulo do Vale <100259992+JPDovale@users.noreply.github.com>
2023-12-16 20:13:22 -0300

    [refactor] remove mock implementations

--- src/infra/database/database.module.ts
+++ src/infra/database/database.module.ts
@@ -1,19 +1,16 @@
 import { Module } from '@nestjs/common'
 import { PrismaService } from './prisma/index.service'
-import { FakersPrismaRepository } from './prisma/repositories/fakers/fakersPrismaRepository'
-import { FakersRepository } from '@modules/faker/repositories/fakersRepository'
-import { TablesRepository } from '@modules/table/repositories/tablesRepository'
-import { TablesPrismaRepository } from './prisma/repositories/tables/tablesPrismaRepository'
-import { RealsRepository } from '@modules/real/repositories/realsRepository'
-import { RealsPrismaRepository } from './prisma/repositories/reals/realsPrismaRepository'
+import { AdministratorRepository } from '@modules/administrator/repositories/AdministratorRepository'
+import { AdministratorPrismaRepository } from './prisma/repositories/administrator/AdministratorPrismaRepository'
 
 @Module({
   providers: [
     PrismaService,
-    { provide: FakersRepository, useClass: FakersPrismaRepository },
-    { provide: TablesRepository, useClass: TablesPrismaRepository },
-    { provide: RealsRepository, useClass: RealsPrismaRepository },
+    {
+      provide: AdministratorRepository,
+      useClass: AdministratorPrismaRepository,
+    },
   ],
-  exports: [PrismaService, FakersRepository, TablesRepository, RealsRepository],
+  exports: [PrismaService, AdministratorRepository],
 })
 export class DatabaseModule {}
