8851cbed377e3c8cf3b64a00d55b3c54cf68857b
João Paulo do Vale <100259992+JPDovale@users.noreply.github.com>
2023-12-16 20:22:32 -0300

    [update]

--- src/shared/errors/PermissionDenied.ts
+++ src/shared/errors/PermissionDenied.ts
@@ -1,7 +1,7 @@
 import { ServiceError } from '@shared/core/error/ServiceError'
 
 export class PermissionDenied extends Error implements ServiceError {
-  status = 401
+  status = 403
 
   constructor() {
     super('Permission denied')
