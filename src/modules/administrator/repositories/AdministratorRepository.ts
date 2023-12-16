221378fca1cedc9d57fde55cbc8017ce58be4da2
João Paulo do Vale <100259992+JPDovale@users.noreply.github.com>
2023-12-16 20:17:39 -0300

    [add] administrator repository contract

---
+++ src/modules/administrator/repositories/AdministratorRepository.ts
@@ -0,0 +1,7 @@
+import { Administrator } from '../entities/Administrator'
+
+export abstract class AdministratorRepository {
+  abstract create(administrator: Administrator): Promise<void>
+  abstract findByLogin(login: string): Promise<Administrator | null>
+  abstract findById(id: string): Promise<Administrator | null>
+}
