4a9f91e0a0d266f842e80e30152d8964e419e37f
João Paulo do Vale <100259992+JPDovale@users.noreply.github.com>
2023-12-20 15:25:00 -0300

    [add] create refresh administrator token repository

--- src/infra/database/database.module.ts
+++ src/infra/database/database.module.ts
@@ -2,6 +2,8 @@ import { Module } from '@nestjs/common'
 import { PrismaService } from './prisma/index.service'
 import { AdministratorRepository } from '@modules/administrator/repositories/AdministratorRepository'
 import { AdministratorPrismaRepository } from './prisma/repositories/administrator/AdministratorPrismaRepository'
+import { RefreshAdministratorTokenRepository } from '@modules/administrator/repositories/RefreshAdministratorTokenRepository'
+import { RefreshAdministratorTokenPrismaRepository } from './prisma/repositories/administrator/RefreshAdministratorTokenPrismaRepository'
 
 @Module({
   providers: [
@@ -10,7 +12,15 @@ import { AdministratorPrismaRepository } from './prisma/repositories/administrat
       provide: AdministratorRepository,
       useClass: AdministratorPrismaRepository,
     },
+    {
+      provide: RefreshAdministratorTokenRepository,
+      useClass: RefreshAdministratorTokenPrismaRepository,
+    },
   ],
-  exports: [PrismaService, AdministratorRepository],
+  exports: [
+    PrismaService,
+    AdministratorRepository,
+    RefreshAdministratorTokenRepository,
+  ],
 })
 export class DatabaseModule {}
---
+++ src/infra/database/prisma/repositories/administrator/RefreshAdministratorTokenPrismaMapper.ts
@@ -0,0 +1,31 @@
+import { RefreshAdministratorToken } from '@modules/administrator/entities/RefreshAdministratorToken'
+import { Prisma, RefreshToken } from '@prisma/client'
+import { UniqueEntityId } from '@shared/core/entities/valueObjects/UniqueEntityId'
+
+export class RefreshAdministratorTokenPrismaMapper {
+  static toEntity(raw: RefreshToken): RefreshAdministratorToken {
+    return RefreshAdministratorToken.create(
+      {
+        administratorId: new UniqueEntityId(raw.administratorId!),
+        expiresIn: raw.expiresIn,
+        token: raw.token,
+        createdAt: raw.createdAt,
+        expiredAt: raw.expiredAt,
+      },
+      new UniqueEntityId(raw.id),
+    )
+  }
+
+  static toPrisma(
+    refreshAdministratorToken: RefreshAdministratorToken,
+  ): Prisma.RefreshTokenUncheckedCreateInput {
+    return {
+      expiresIn: refreshAdministratorToken.expiresIn,
+      token: refreshAdministratorToken.token,
+      administratorId: refreshAdministratorToken.administratorId.toString(),
+      createdAt: refreshAdministratorToken.createdAt,
+      expiredAt: refreshAdministratorToken.expiredAt,
+      id: refreshAdministratorToken.id.toString(),
+    }
+  }
+}
