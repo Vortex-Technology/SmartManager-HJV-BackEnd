e3d950e24e1238ce158e70fedd21dde65ff6eff9
João Paulo do Vale <100259992+JPDovale@users.noreply.github.com>
2023-12-20 15:28:44 -0300

    [add] login administrator on module

--- src/modules/administrator/administrator.module.ts
+++ src/modules/administrator/administrator.module.ts
@@ -3,10 +3,14 @@ import { CreateAdministratorService } from './services/createAdministrator.servi
 import { DatabaseModule } from '@infra/database/database.module'
 import { CryptographyModule } from '@providers/cryptography/cryptography.module'
 import { CreateAdministratorController } from './controllers/createAdministrator.controller'
+import { LoginAdministratorService } from './services/loginAdministrator.service'
+import { EnvModule } from '@infra/env/env.module'
+import { DateModule } from '@providers/date/date.module'
+import { LoginAdministratorController } from './controllers/loginAdministrator.controller'
 
 @Module({
-  controllers: [CreateAdministratorController],
-  imports: [DatabaseModule, CryptographyModule],
-  providers: [CreateAdministratorService],
+  controllers: [CreateAdministratorController, LoginAdministratorController],
+  imports: [DatabaseModule, CryptographyModule, DateModule, EnvModule],
+  providers: [CreateAdministratorService, LoginAdministratorService],
 })
 export class AdministratorModule {}
