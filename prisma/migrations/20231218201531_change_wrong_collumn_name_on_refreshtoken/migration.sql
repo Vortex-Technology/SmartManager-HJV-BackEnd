43e1209ad16044c4e42dc86ba4465cae52d86268
João Paulo do Vale <100259992+JPDovale@users.noreply.github.com>
2023-12-20 15:20:31 -0300

    [migrations] Create table refresh token

---
+++ prisma/migrations/20231218201531_change_wrong_collumn_name_on_refreshtoken/migration.sql
@@ -0,0 +1,9 @@
+/*
+  Warnings:
+
+  - You are about to drop the column `expires_at` on the `refresh_tokens` table. All the data in the column will be lost.
+
+*/
+-- AlterTable
+ALTER TABLE "refresh_tokens" DROP COLUMN "expires_at",
+ADD COLUMN     "expired_at" TIMESTAMP(3);
