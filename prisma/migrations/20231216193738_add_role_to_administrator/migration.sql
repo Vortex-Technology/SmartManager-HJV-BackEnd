9a76d7b7fc78df89341d46ad5482a00e9e6c9939
João Paulo do Vale <100259992+JPDovale@users.noreply.github.com>
2023-12-26 18:19:06 -0300

    [refactor] recreate database

--- prisma/migrations/20231216193738_add_role_to_administrator/migration.sql
+++
@@ -1,5 +0,0 @@
--- CreateEnum
-CREATE TYPE "RoleAdministrator" AS ENUM ('MASTER', 'FULL_ACCESS', 'CREATOR', 'VIEWER');
-
--- AlterTable
-ALTER TABLE "administrator" ADD COLUMN     "role" "RoleAdministrator" NOT NULL DEFAULT 'FULL_ACCESS';
---
+++ prisma/migrations/20231226210754_recreate_database/migration.sql
@@ -0,0 +1,44 @@
+-- CreateEnum
+CREATE TYPE "RoleCollaborator" AS ENUM ('MASTER', 'FULL_ACCESS', 'EDITOR', 'VIEWER');
+
+-- CreateEnum
+CREATE TYPE "CollaboratorType" AS ENUM ('ADMINISTRATOR', 'SALLER', 'ATTENDANT');
+
+-- CreateTable
+CREATE TABLE "refresh_tokens" (
+    "id" TEXT NOT NULL,
+    "token" TEXT NOT NULL,
+    "expires_in" TIMESTAMP(3) NOT NULL,
+    "expired_at" TIMESTAMP(3),
+    "created_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "collaborator_id" TEXT,
+
+    CONSTRAINT "refresh_tokens_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "collaborators" (
+    "id" TEXT NOT NULL,
+    "name" TEXT NOT NULL,
+    "image" TEXT,
+    "login" TEXT NOT NULL,
+    "password" TEXT NOT NULL,
+    "type" "CollaboratorType" NOT NULL DEFAULT 'ATTENDANT',
+    "role" "RoleCollaborator" NOT NULL DEFAULT 'MASTER',
+    "created_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "udpated_at" TIMESTAMP(3),
+
+    CONSTRAINT "collaborators_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "refresh_tokens_id_key" ON "refresh_tokens"("id");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "collaborators_id_key" ON "collaborators"("id");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "collaborators_login_key" ON "collaborators"("login");
+
+-- AddForeignKey
+ALTER TABLE "refresh_tokens" ADD CONSTRAINT "refresh_tokens_collaborator_id_fkey" FOREIGN KEY ("collaborator_id") REFERENCES "collaborators"("id") ON DELETE SET NULL ON UPDATE CASCADE;
