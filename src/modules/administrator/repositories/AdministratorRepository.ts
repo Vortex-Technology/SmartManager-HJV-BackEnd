8789898ebac9a8609ee09d654585b5638f5b044c
João Paulo do Vale <100259992+JPDovale@users.noreply.github.com>
2023-12-26 22:26:15 -0300

    [add] save method on repository

--- src/modules/administrator/repositories/AdministratorRepository.ts
+++ src/modules/administrator/repositories/AdministratorRepository.ts
@@ -4,4 +4,5 @@ export abstract class AdministratorRepository {
   abstract create(administrator: Administrator): Promise<void>
   abstract findByLogin(login: string): Promise<Administrator | null>
   abstract findById(id: string): Promise<Administrator | null>
+  abstract save(administrator: Administrator): Promise<void>
 }
