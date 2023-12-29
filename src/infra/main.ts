559f5dc14114d6d927107a8b48ce7a76544d3541
João Paulo do Vale <100259992+JPDovale@users.noreply.github.com>
2023-12-29 20:19:02 -0300

    [add] cors options and init socket server

--- src/infra/main.ts
+++ src/infra/main.ts
@@ -2,14 +2,31 @@ import 'newrelic'
 import { NestFactory } from '@nestjs/core'
 import { AppModule } from './app.module'
 import { EnvService } from './env/env.service'
+import { createServer } from 'http'
+import { IoAdapter } from '@nestjs/platform-socket.io'
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, {
-    // logger: false
+    // logger: false,
+    cors: {
+      origin: 'http://localhost:1212',
+      credentials: true,
+    },
   })
 
   const envService = app.get(EnvService)
   const port = envService.get('PORT')
+
+  const server = createServer(app.getHttpAdapter().getInstance())
+
+  const io = require('socket.io')(server, {
+    cors: {
+      origin: 'http://localhost:1212',
+      credentials: true,
+    },
+  })
+
+  app.useWebSocketAdapter(new IoAdapter(io))
 
   await app.listen(port)
 }
