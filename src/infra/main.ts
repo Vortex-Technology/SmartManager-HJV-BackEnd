50b31c0d2b542a6401bec7f8334466ef407eb06c
João Paulo do Vale <100259992+JPDovale@users.noreply.github.com>
2023-12-29 20:59:08 -0300

    [delete] wrong socket initalizaton

--- src/infra/main.ts
+++ src/infra/main.ts
@@ -2,8 +2,6 @@ import 'newrelic'
 import { NestFactory } from '@nestjs/core'
 import { AppModule } from './app.module'
 import { EnvService } from './env/env.service'
-import { createServer } from 'http'
-import { IoAdapter } from '@nestjs/platform-socket.io'
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, {
@@ -16,17 +14,6 @@ async function bootstrap() {
 
   const envService = app.get(EnvService)
   const port = envService.get('PORT')
-
-  const server = createServer(app.getHttpAdapter().getInstance())
-
-  const io = require('socket.io')(server, {
-    cors: {
-      origin: 'http://localhost:1212',
-      credentials: true,
-    },
-  })
-
-  app.useWebSocketAdapter(new IoAdapter(io))
 
   await app.listen(port)
 }
