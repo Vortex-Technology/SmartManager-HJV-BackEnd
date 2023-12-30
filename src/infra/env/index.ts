8ed54c3207c4dbfe38734aa2c0abad503b85725f
João Paulo do Vale <100259992+JPDovale@users.noreply.github.com>
2023-12-29 21:09:50 -0300

    [add] in env socket port

--- src/infra/env/index.ts
+++ src/infra/env/index.ts
@@ -3,6 +3,7 @@ import { z } from 'zod'
 export const envSchema = z.object({
   DATABASE_URL: z.string().url(),
   PORT: z.coerce.number().optional().default(3030),
+  SOCKET_PORT: z.coerce.number().optional().default(3031),
   JWT_PRIVATE_kEY: z.string(),
   JWT_PUBLIC_KEY: z.string(),
   JWT_USER_ACCESS_EXPIRES_IN: z.string(),
