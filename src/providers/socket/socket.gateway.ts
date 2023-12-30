d089dde182d2e6a1bbb8bacaf5fed1b259b8ea5c
João Paulo do Vale <100259992+JPDovale@users.noreply.github.com>
2023-12-29 21:10:12 -0300

    [add] deferent running socket server environment

--- src/providers/socket/socket.gateway.ts
+++ src/providers/socket/socket.gateway.ts
@@ -1,3 +1,5 @@
+import { EnvService } from '@infra/env/env.service'
+import { Injectable } from '@nestjs/common'
 import {
   OnGatewayConnection,
   OnGatewayInit,
@@ -6,9 +8,11 @@ import {
   WebSocketServer,
   ConnectedSocket,
 } from '@nestjs/websockets'
+import { createServer } from 'http'
 import { Server, Socket } from 'socket.io'
 
-@WebSocketGateway(3005, {
+@Injectable()
+@WebSocketGateway({
   transports: ['websocket', 'polling'],
   cors: {
     origin: 'http://localhost:1212',
@@ -18,9 +22,58 @@ import { Server, Socket } from 'socket.io'
 export class SocketGateway implements OnGatewayInit, OnGatewayConnection {
   @WebSocketServer() server!: Server
 
-  afterInit(server: Server) {
+  constructor(private readonly env: EnvService) {}
+
+  async afterInit(server: Server) {
+    const runningInEnv = this.env.get('NODE_ENV')
+    const socketPort = this.env.get('SOCKET_PORT')
+
     this.server = server
-    console.log('Socket server running')
+
+    if (runningInEnv === 'test') {
+      const testPort = await this.findAvailablePort()
+      server.listen(testPort)
+      return console.log(`Socket server running on port ${testPort}`)
+    }
+
+    server.listen(socketPort)
+    console.log(`Socket server running on port ${socketPort}`)
+  }
+
+  async findAvailablePort(
+    startingPort = 3006,
+    maxAttempts = 100,
+  ): Promise<number> {
+    let port = startingPort
+    let attempts = 0
+
+    while (attempts < maxAttempts) {
+      if (await this.isPortAvailable(port)) {
+        return port
+      }
+
+      port++
+      attempts++
+    }
+
+    throw new Error('Unable to find an available port for WebSocket tests')
+  }
+
+  async isPortAvailable(port: number): Promise<boolean> {
+    return new Promise<boolean>((resolve) => {
+      const server = createServer()
+
+      server.once('error', () => {
+        resolve(false)
+      })
+
+      server.once('listening', () => {
+        server.close()
+        resolve(true)
+      })
+
+      server.listen(port, '127.0.0.1')
+    })
   }
 
   // temporary
