9f81e7a0e6ad2a86199a85b6b451edd53f85c927
João Paulo do Vale <100259992+JPDovale@users.noreply.github.com>
2023-12-29 21:10:28 -0300

    [add] env import to socket module

--- src/providers/socket/socket.module.ts
+++ src/providers/socket/socket.module.ts
@@ -1,7 +1,9 @@
 import { Module } from '@nestjs/common'
 import { SocketGateway } from './socket.gateway'
+import { EnvModule } from '@infra/env/env.module'
 
 @Module({
+  imports: [EnvModule],
   providers: [SocketGateway],
   exports: [SocketGateway],
 })
