b855e76faf683e1d67d78787f2027d1b279b658e
João Paulo do Vale <100259992+JPDovale@users.noreply.github.com>
2024-01-05 17:13:38 -0300

    [delete] duplicated refresh token entities and implementations

--- src/modules/attendant/repositories/RefreshAttendantTokenRepository.ts
+++
@@ -1,5 +0,0 @@
-import { RefreshAttendantToken } from '../entities/RefreshAttendantToken'
-
-export abstract class RefreshAttendantTokenRepository {
-  abstract create(refreshAttendantToken: RefreshAttendantToken): Promise<void>
-}
--- src/modules/seller/repositories/RefreshSellerTokenRepository.ts
+++
@@ -1,5 +0,0 @@
-import { RefreshSellerToken } from '../entities/RefreshSellerToken'
-
-export abstract class RefreshSellerTokenRepository {
-  abstract create(refreshSellerToken: RefreshSellerToken): Promise<void>
-}
