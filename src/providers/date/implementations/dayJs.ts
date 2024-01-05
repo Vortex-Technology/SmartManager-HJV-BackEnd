0ba4a5da582802465e7e595d37e99bc9766a46a1
João Paulo do Vale <100259992+JPDovale@users.noreply.github.com>
2024-01-05 17:20:02 -0300

    [add] date verifications

--- src/providers/date/implementations/dayJs.ts
+++ src/providers/date/implementations/dayJs.ts
@@ -3,16 +3,27 @@ import { DateAddition } from '../contracts/dateAddition'
 // Don't touch this
 import * as dayjsProd from 'dayjs'
 import dayjsTest from 'dayjs'
+import { DateVerifications } from '../contracts/dateVerifications'
 
 const dayjs = process.env.NODE_ENV === 'test' ? dayjsTest : dayjsProd
 
 @Injectable()
-export class DayJs implements DateAddition {
+export class DayJs implements DateAddition, DateVerifications {
   addDaysInCurrentDate(days: number): Date {
     return dayjs().add(days, 'days').toDate()
   }
 
   addDayInCurrentDate(): Date {
     return this.addDaysInCurrentDate(1)
+  }
+
+  isBefore({
+    startDate = new Date(),
+    endDate,
+  }: {
+    startDate?: Date
+    endDate: Date
+  }): boolean {
+    return dayjs(startDate).isBefore(endDate)
   }
 }
