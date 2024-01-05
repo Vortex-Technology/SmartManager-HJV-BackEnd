c04eda5bda6e7ad1b6a0f7a577e60d01cf3ce86a
João Paulo do Vale <100259992+JPDovale@users.noreply.github.com>
2024-01-05 17:16:15 -0300

    [refactor] database module to use default refresh token repository

--- src/infra/database/database.module.ts
+++ src/infra/database/database.module.ts
@@ -2,22 +2,18 @@ import { Module } from '@nestjs/common'
 import { PrismaService } from './prisma/index.service'
 import { AdministratorRepository } from '@modules/administrator/repositories/AdministratorRepository'
 import { AdministratorPrismaRepository } from './prisma/repositories/administrator/AdministratorPrismaRepository'
-import { RefreshAdministratorTokenRepository } from '@modules/administrator/repositories/RefreshAdministratorTokenRepository'
-import { RefreshAdministratorTokenPrismaRepository } from './prisma/repositories/administrator/RefreshAdministratorTokenPrismaRepository'
 import { SellerRepository } from '@modules/seller/repositories/SellerRepository'
 import { SellerPrismaRepository } from './prisma/repositories/seller/SellerPrismaRepository'
 import { AttendantRepository } from '@modules/attendant/repositories/AttendantRepository'
 import { AttendantPrismaRepository } from './prisma/repositories/attendant/AttendantPrismaRepository'
-import { RefreshSellerTokenRepository } from '@modules/seller/repositories/RefreshSellerTokenRepository'
-import { RefreshSellerTokenPrismaRepository } from './prisma/repositories/seller/RefreshSellerTokenPrismaRepository'
-import { RefreshAttendantTokenRepository } from '@modules/attendant/repositories/RefreshAttendantTokenRepository'
-import { RefreshAttendantTokenPrismaRepository } from './prisma/repositories/attendant/RefreshAttendantTokenPrismaRepository'
 import { ProductCategoriesRepository } from '@modules/product/repositories/ProductCategoriesRepository'
 import { ProductCategoryPrismaRepository } from './prisma/repositories/product/ProductCategoryPrismaRepository'
 import { ProductVariantsRepository } from '@modules/product/repositories/ProductVariantsRepository'
 import { ProductVariantPrismaRepository } from './prisma/repositories/product/ProductVariantPrismaRepository'
 import { ProductsRepository } from '@modules/product/repositories/ProductsRepository'
 import { ProductPrismaRepository } from './prisma/repositories/product/ProductPrismaRepository'
+import { RefreshTokenRepository } from '@modules/refreshToken/repositories/RefreshTokenRepository'
+import { RefreshTokenPrismaRepository } from './prisma/repositories/refreshToken/RefreshTokenPrismaRepository'
 
 @Module({
   providers: [
@@ -25,10 +21,6 @@ import { ProductPrismaRepository } from './prisma/repositories/product/ProductPr
     {
       provide: AdministratorRepository,
       useClass: AdministratorPrismaRepository,
-    },
-    {
-      provide: RefreshAdministratorTokenRepository,
-      useClass: RefreshAdministratorTokenPrismaRepository,
     },
     {
       provide: SellerRepository,
@@ -39,12 +31,8 @@ import { ProductPrismaRepository } from './prisma/repositories/product/ProductPr
       useClass: AttendantPrismaRepository,
     },
     {
-      provide: RefreshSellerTokenRepository,
-      useClass: RefreshSellerTokenPrismaRepository,
-    },
-    {
-      provide: RefreshAttendantTokenRepository,
-      useClass: RefreshAttendantTokenPrismaRepository,
+      provide: RefreshTokenRepository,
+      useClass: RefreshTokenPrismaRepository,
     },
     {
       provide: ProductCategoriesRepository,
@@ -62,11 +50,9 @@ import { ProductPrismaRepository } from './prisma/repositories/product/ProductPr
   exports: [
     PrismaService,
     AdministratorRepository,
-    RefreshAdministratorTokenRepository,
     SellerRepository,
     AttendantRepository,
-    RefreshSellerTokenRepository,
-    RefreshAttendantTokenRepository,
+    RefreshTokenRepository,
     ProductCategoriesRepository,
     ProductVariantsRepository,
     ProductsRepository,
