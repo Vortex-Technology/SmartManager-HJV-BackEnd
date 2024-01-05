37d9aafc75196f5d9696ca28a76dda83e417dbad
João Paulo do Vale <100259992+JPDovale@users.noreply.github.com>
2024-01-05 17:20:34 -0300

    [add] refresh token service

---
+++ src/modules/refreshToken/services/refreshToken.service.ts
@@ -0,0 +1,145 @@
+import { Either, left, right } from '@shared/core/error/Either'
+import { SessionExpired } from '../errors/SessionExpired'
+import { Injectable } from '@nestjs/common'
+import { RefreshTokenRepository } from '../repositories/RefreshTokenRepository'
+import { Encrypter } from '@providers/cryptography/contracts/encrypter'
+import { EnvService } from '@infra/env/env.service'
+import { Decoder } from '@providers/cryptography/contracts/decoder'
+import { AdministratorRepository } from '@modules/administrator/repositories/AdministratorRepository'
+import { SellerRepository } from '@modules/seller/repositories/SellerRepository'
+import { AttendantRepository } from '@modules/attendant/repositories/AttendantRepository'
+import { CollaboratorNotFound } from '../errors/CollaboratorNotFound'
+import { DateVerifications } from '@providers/date/contracts/dateVerifications'
+import { Administrator } from '@modules/administrator/entities/Administrator'
+import { RefreshToken } from '../entities/RefreshToken'
+import { DateAddition } from '@providers/date/contracts/dateAddition'
+
+interface Request {
+  refreshToken: string
+}
+
+type Response = Either<
+  SessionExpired | CollaboratorNotFound,
+  {
+    accessToken: string
+    refreshToken: string
+  }
+>
+
+@Injectable()
+export class RefreshTokenService {
+  constructor(
+    private readonly administratorRepository: AdministratorRepository,
+    private readonly refreshTokenRepository: RefreshTokenRepository,
+    private readonly attendantRepository: AttendantRepository,
+    private readonly dateVerifications: DateVerifications,
+    private readonly sellerRepository: SellerRepository,
+    private readonly dateAddition: DateAddition,
+    private readonly encrypter: Encrypter,
+    private readonly decoder: Decoder,
+    private readonly env: EnvService,
+  ) {}
+
+  async execute({ refreshToken }: Request): Promise<Response> {
+    const { isValid, payload } = await this.decoder.decrypt(refreshToken)
+
+    if (!isValid) {
+      return left(new SessionExpired())
+    }
+
+    if (!payload) {
+      return left(new SessionExpired())
+    }
+
+    const { sub, type } = payload
+    const findCollaboratorMapper = {
+      ADMINISTRATOR: (id: string) => this.administratorRepository.findById(id),
+      SELLER: (id: string) => this.sellerRepository.findById(id),
+      ATTENDANT: (id: string) => this.attendantRepository.findById(id),
+    }
+
+    if (!findCollaboratorMapper[type]) {
+      return left(new CollaboratorNotFound(type.toLocaleLowerCase()))
+    }
+
+    const collaborator = await findCollaboratorMapper[type](sub)
+
+    if (!collaborator) {
+      return left(new CollaboratorNotFound(type.toLocaleLowerCase()))
+    }
+
+    const collaboratorToken =
+      await this.refreshTokenRepository.findByCollaboratorIdAndRefreshToken({
+        collaboratorId: collaborator.id.toString(),
+        refreshToken,
+      })
+
+    if (!collaboratorToken) {
+      return left(new SessionExpired())
+    }
+
+    const tokenIsNotExpired = this.dateVerifications.isBefore({
+      endDate: collaboratorToken.expiresIn,
+    })
+
+    await this.refreshTokenRepository.permanentlyDeleteByCollaboratorId(
+      collaborator.id.toString(),
+    )
+
+    if (!tokenIsNotExpired) {
+      return left(new SessionExpired())
+    }
+
+    const jwtRefreshExpires =
+      collaborator instanceof Administrator
+        ? this.env.get('JWT_ADM_REFRESH_EXPIRES_IN')
+        : this.env.get('JWT_USER_REFRESH_EXPIRES_IN')
+
+    const refreshExpires =
+      collaborator instanceof Administrator
+        ? this.env.get('ADM_REFRESH_EXPIRES_IN')
+        : this.env.get('USER_REFRESH_EXPIRES_IN')
+
+    const jwtAccessExpires =
+      collaborator instanceof Administrator
+        ? this.env.get('JWT_ADM_ACCESS_EXPIRES_IN')
+        : this.env.get('JWT_USER_ACCESS_EXPIRES_IN')
+
+    const revalidatedRefreshToken = await this.encrypter.encrypt(
+      {
+        sub: collaborator.id.toString(),
+        type,
+        role:
+          collaborator instanceof Administrator ? collaborator.role : undefined,
+      },
+      {
+        expiresIn: jwtRefreshExpires,
+      },
+    )
+
+    const accessToken = await this.encrypter.encrypt(
+      {
+        sub: collaborator.id.toString(),
+        type,
+        role:
+          collaborator instanceof Administrator ? collaborator.role : undefined,
+      },
+      {
+        expiresIn: jwtAccessExpires,
+      },
+    )
+
+    const newRefreshToken = RefreshToken.create({
+      collaboratorId: collaborator.id,
+      token: revalidatedRefreshToken,
+      expiresIn: this.dateAddition.addDaysInCurrentDate(refreshExpires),
+    })
+
+    await this.refreshTokenRepository.create(newRefreshToken)
+
+    return right({
+      accessToken,
+      refreshToken: revalidatedRefreshToken,
+    })
+  }
+}
---
+++ src/modules/refreshToken/services/refreshToken.spec.ts
@@ -0,0 +1,345 @@
+import { AdministratorInMemoryRepository } from '@test/repositories/modules/administrator/AdministratorInMemoryRepository'
+import { makeAdministrator } from '@test/factories/modules/administrator/makeAdministrator'
+import { FakeEncrypter } from '@test/repositories/providers/cryptography/fakeEncrypter'
+import { FakeEnv } from '@test/config/env/fakeEnv'
+import { DayJs } from '@providers/date/implementations/dayJs'
+import { EnvService } from '@infra/env/env.service'
+import { RefreshTokenInMemoryRepository } from '@test/repositories/modules/refreshToken/RefreshTokenInMemoryRepository'
+import { RefreshTokenService } from './refreshToken.service'
+import { AttendantInMemoryRepository } from '@test/repositories/modules/attendant/AttendantInMemoryRepository'
+import { SellerInMemoryRepository } from '@test/repositories/modules/seller/SellerInMemoryRepository'
+import { makeRefreshToken } from '@test/factories/modules/refreshToken/makeRefreshToken'
+import { makeAttendant } from '@test/factories/modules/attendant/makeAttendant'
+import dayjs from 'dayjs'
+import { makeSeller } from '@test/factories/modules/seller/makeSeller'
+import { fakerPT_BR } from '@faker-js/faker'
+import { SessionExpired } from '../errors/SessionExpired'
+import { UniqueEntityId } from '@shared/core/entities/valueObjects/UniqueEntityId'
+import { CollaboratorNotFound } from '../errors/CollaboratorNotFound'
+
+let fakeEncrypter: FakeEncrypter
+let fakeEnv: FakeEnv
+let fakeDateProvider: DayJs
+let refreshTokenInMemoryRepository: RefreshTokenInMemoryRepository
+let administratorInMemoryRepository: AdministratorInMemoryRepository
+let attendantInMemoryRepository: AttendantInMemoryRepository
+let sellerInMemoryRepository: SellerInMemoryRepository
+
+let sut: RefreshTokenService
+
+describe('Refresh Token', () => {
+  beforeEach(() => {
+    fakeEncrypter = new FakeEncrypter()
+    fakeEnv = new FakeEnv()
+    fakeDateProvider = new DayJs()
+    refreshTokenInMemoryRepository = new RefreshTokenInMemoryRepository()
+    administratorInMemoryRepository = new AdministratorInMemoryRepository()
+    attendantInMemoryRepository = new AttendantInMemoryRepository()
+    sellerInMemoryRepository = new SellerInMemoryRepository()
+
+    sut = new RefreshTokenService(
+      administratorInMemoryRepository,
+      refreshTokenInMemoryRepository,
+      attendantInMemoryRepository,
+      fakeDateProvider,
+      sellerInMemoryRepository,
+      fakeDateProvider,
+      fakeEncrypter,
+      fakeEncrypter,
+      fakeEnv as EnvService,
+    )
+  })
+
+  it('should be able to refresh a token of administrator', async () => {
+    const administrator = makeAdministrator()
+
+    const token = await fakeEncrypter.encrypt({
+      sub: administrator.id.toString(),
+      type: 'ADMINISTRATOR',
+    })
+
+    const refreshToken = makeRefreshToken({
+      collaboratorId: administrator.id,
+      token,
+      expiresIn: fakeDateProvider.addDaysInCurrentDate(1),
+    })
+
+    await administratorInMemoryRepository.create(administrator)
+    await refreshTokenInMemoryRepository.create(refreshToken)
+
+    const response = await sut.execute({
+      refreshToken: token,
+    })
+
+    expect(response.isRight()).toBe(true)
+
+    if (response.isRight()) {
+      expect(response.value).toEqual(
+        expect.objectContaining({
+          accessToken: expect.any(String),
+          refreshToken: expect.any(String),
+        }),
+      )
+      expect(refreshTokenInMemoryRepository.refreshTokens).toHaveLength(1)
+
+      const tokenDecoded = await fakeEncrypter.decrypt(
+        response.value.refreshToken,
+      )
+
+      expect(tokenDecoded).toBeTruthy()
+      expect(tokenDecoded.payload?.type).toEqual('ADMINISTRATOR')
+
+      const refreshToken = refreshTokenInMemoryRepository.refreshTokens[0]
+
+      const receivedDate = dayjs(refreshToken.expiresIn)
+      const expectedDate = fakeDateProvider.addDaysInCurrentDate(
+        fakeEnv.get('ADM_REFRESH_EXPIRES_IN'),
+      )
+
+      expect(receivedDate.isSame(expectedDate, 'seconds')).toEqual(true)
+    }
+  })
+
+  it('should be able to refresh a token of attendant', async () => {
+    const attendant = makeAttendant()
+
+    const token = await fakeEncrypter.encrypt({
+      sub: attendant.id.toString(),
+      type: 'ATTENDANT',
+    })
+
+    const refreshToken = makeRefreshToken({
+      collaboratorId: attendant.id,
+      token,
+      expiresIn: fakeDateProvider.addDaysInCurrentDate(1),
+    })
+
+    await attendantInMemoryRepository.create(attendant)
+    await refreshTokenInMemoryRepository.create(refreshToken)
+
+    const response = await sut.execute({
+      refreshToken: token,
+    })
+
+    expect(response.isRight()).toBe(true)
+
+    if (response.isRight()) {
+      expect(response.value).toEqual(
+        expect.objectContaining({
+          accessToken: expect.any(String),
+          refreshToken: expect.any(String),
+        }),
+      )
+      expect(refreshTokenInMemoryRepository.refreshTokens).toHaveLength(1)
+
+      const tokenDecoded = await fakeEncrypter.decrypt(
+        response.value.refreshToken,
+      )
+
+      expect(tokenDecoded).toBeTruthy()
+      expect(tokenDecoded.payload?.type).toEqual('ATTENDANT')
+
+      const refreshToken = refreshTokenInMemoryRepository.refreshTokens[0]
+
+      const receivedDate = dayjs(refreshToken.expiresIn)
+      const expectedDate = fakeDateProvider.addDaysInCurrentDate(
+        fakeEnv.get('USER_REFRESH_EXPIRES_IN'),
+      )
+
+      expect(receivedDate.isSame(expectedDate, 'seconds')).toEqual(true)
+    }
+  })
+
+  it('should be able to refresh a token of seller', async () => {
+    const seller = makeSeller()
+
+    const token = await fakeEncrypter.encrypt({
+      sub: seller.id.toString(),
+      type: 'SELLER',
+    })
+
+    const refreshToken = makeRefreshToken({
+      collaboratorId: seller.id,
+      token,
+      expiresIn: fakeDateProvider.addDaysInCurrentDate(1),
+    })
+
+    await sellerInMemoryRepository.create(seller)
+    await refreshTokenInMemoryRepository.create(refreshToken)
+
+    const response = await sut.execute({
+      refreshToken: token,
+    })
+
+    expect(response.isRight()).toBe(true)
+
+    if (response.isRight()) {
+      expect(response.value).toEqual(
+        expect.objectContaining({
+          accessToken: expect.any(String),
+          refreshToken: expect.any(String),
+        }),
+      )
+      expect(refreshTokenInMemoryRepository.refreshTokens).toHaveLength(1)
+
+      const tokenDecoded = await fakeEncrypter.decrypt(
+        response.value.refreshToken,
+      )
+
+      expect(tokenDecoded).toBeTruthy()
+      expect(tokenDecoded.payload?.type).toEqual('SELLER')
+
+      const refreshToken = refreshTokenInMemoryRepository.refreshTokens[0]
+
+      const receivedDate = dayjs(refreshToken.expiresIn)
+      const expectedDate = fakeDateProvider.addDaysInCurrentDate(
+        fakeEnv.get('USER_REFRESH_EXPIRES_IN'),
+      )
+
+      expect(receivedDate.isSame(expectedDate, 'seconds')).toEqual(true)
+    }
+  })
+
+  it('not should be able to refresh a token if token already expired', async () => {
+    const administrator = makeAdministrator()
+    const token = await fakeEncrypter.encrypt({
+      sub: administrator.id.toString(),
+      type: 'ADMINISTRATOR',
+    })
+
+    const refreshToken = makeRefreshToken({
+      collaboratorId: administrator.id,
+      token,
+      expiresIn: fakerPT_BR.date.past(),
+    })
+
+    await administratorInMemoryRepository.create(administrator)
+    await refreshTokenInMemoryRepository.create(refreshToken)
+
+    const response = await sut.execute({
+      refreshToken: token,
+    })
+
+    expect(response.isLeft()).toBe(true)
+    expect(response.value).toBeInstanceOf(SessionExpired)
+    expect(refreshTokenInMemoryRepository.refreshTokens).toHaveLength(0)
+  })
+
+  it('not should be able to refresh a token if collaborator not exists', async () => {
+    const token = await fakeEncrypter.encrypt({
+      sub: 'inexistent-id',
+      type: 'ADMINISTRATOR',
+    })
+
+    const refreshToken = makeRefreshToken({
+      collaboratorId: new UniqueEntityId('inexistent-id'),
+      token,
+      expiresIn: fakerPT_BR.date.past(),
+    })
+
+    await refreshTokenInMemoryRepository.create(refreshToken)
+
+    const response = await sut.execute({
+      refreshToken: token,
+    })
+
+    expect(response.isLeft()).toBe(true)
+    expect(response.value).toBeInstanceOf(CollaboratorNotFound)
+    expect(refreshTokenInMemoryRepository.refreshTokens).toHaveLength(1)
+  })
+
+  it('not should be able to refresh a token if token have inexistent type', async () => {
+    const administrator = makeAdministrator()
+    const token = await fakeEncrypter.encrypt({
+      sub: administrator.id.toString(),
+      type: 'ADM',
+    })
+
+    const refreshToken = makeRefreshToken({
+      collaboratorId: administrator.id,
+      token,
+      expiresIn: fakerPT_BR.date.past(),
+    })
+
+    await administratorInMemoryRepository.create(administrator)
+    await refreshTokenInMemoryRepository.create(refreshToken)
+
+    const response = await sut.execute({
+      refreshToken: token,
+    })
+
+    expect(response.isLeft()).toBe(true)
+    expect(response.value).toBeInstanceOf(CollaboratorNotFound)
+    expect(refreshTokenInMemoryRepository.refreshTokens).toHaveLength(1)
+  })
+
+  it('not should be able to refresh a token if token not exist', async () => {
+    const administrator = makeAdministrator()
+    const token = await fakeEncrypter.encrypt({
+      sub: administrator.id.toString(),
+      type: 'ADMINISTRATOR',
+    })
+
+    await administratorInMemoryRepository.create(administrator)
+
+    const response = await sut.execute({
+      refreshToken: token,
+    })
+
+    expect(response.isLeft()).toBe(true)
+    expect(response.value).toBeInstanceOf(SessionExpired)
+    expect(refreshTokenInMemoryRepository.refreshTokens).toHaveLength(0)
+  })
+
+  it('should be able to delete permanently all tokens of administrator when a new is created', async () => {
+    const administrator = makeAdministrator()
+    const token = await fakeEncrypter.encrypt({
+      sub: administrator.id.toString(),
+      type: 'ADMINISTRATOR',
+    })
+
+    for (let i = 0; i <= 10; i++) {
+      const refreshToken = makeRefreshToken({
+        collaboratorId: administrator.id,
+        token,
+        expiresIn: fakeDateProvider.addDaysInCurrentDate(1),
+      })
+
+      await refreshTokenInMemoryRepository.create(refreshToken)
+    }
+
+    await administratorInMemoryRepository.create(administrator)
+
+    const response = await sut.execute({
+      refreshToken: token,
+    })
+
+    expect(response.isRight()).toBe(true)
+
+    if (response.isRight()) {
+      expect(response.value).toEqual(
+        expect.objectContaining({
+          accessToken: expect.any(String),
+          refreshToken: expect.any(String),
+        }),
+      )
+      expect(refreshTokenInMemoryRepository.refreshTokens).toHaveLength(1)
+
+      const tokenDecoded = await fakeEncrypter.decrypt(
+        response.value.refreshToken,
+      )
+
+      expect(tokenDecoded).toBeTruthy()
+      expect(tokenDecoded.payload?.type).toEqual('ADMINISTRATOR')
+
+      const refreshToken = refreshTokenInMemoryRepository.refreshTokens[0]
+
+      const receivedDate = dayjs(refreshToken.expiresIn)
+      const expectedDate = fakeDateProvider.addDaysInCurrentDate(
+        fakeEnv.get('ADM_REFRESH_EXPIRES_IN'),
+      )
+
+      expect(receivedDate.isSame(expectedDate, 'seconds')).toEqual(true)
+    }
+  })
+})
