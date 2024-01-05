987ef2ab713169b7aa6d7a88f047b47bc84f757c
João Paulo do Vale <100259992+JPDovale@users.noreply.github.com>
2024-01-05 17:19:43 -0300

    [add] decoder of token

--- src/providers/cryptography/implementations/jwtEncrypter.ts
+++ src/providers/cryptography/implementations/jwtEncrypter.ts
@@ -1,9 +1,11 @@
 import { Injectable } from '@nestjs/common'
 import { Encrypter } from '../contracts/encrypter'
 import { JwtService, JwtSignOptions } from '@nestjs/jwt'
+import { Decoder } from '../contracts/decoder'
+import { TokenPayloadSchema } from '@providers/auth/strategys/jwtStrategy'
 
 @Injectable()
-export class JwtEncrypter implements Encrypter {
+export class JwtEncrypter implements Encrypter, Decoder {
   constructor(private readonly jwtService: JwtService) {}
 
   async encrypt(
@@ -11,5 +13,17 @@ export class JwtEncrypter implements Encrypter {
     options: JwtSignOptions = {},
   ): Promise<string> {
     return await this.jwtService.signAsync(payload, options)
+  }
+
+  async decrypt(
+    token: string,
+  ): Promise<{ payload?: TokenPayloadSchema; isValid: boolean }> {
+    try {
+      const payload = await this.jwtService.verifyAsync(token)
+
+      return { payload, isValid: true }
+    } catch (error) {
+      return { isValid: false }
+    }
   }
 }
