02da8679d51bd80f44eb14924edd4cbdd90ec4f0
JPDovale <joaopaulodovaledeo.60@gmail.com>
2024-01-10 17:42:56 -0300

    [add] methods on attendants repository

--- src/modules/attendant/repositories/AttendantRepository.ts
+++ src/modules/attendant/repositories/AttendantRepository.ts
@@ -4,4 +4,10 @@ export abstract class AttendantRepository {
   abstract create(attendant: Attendant): Promise<void>
   abstract findByLogin(login: string): Promise<Attendant | null>
   abstract findById(id: string): Promise<Attendant | null>
+  abstract findMany(props: {
+    page: number
+    limit: number
+  }): Promise<Attendant[]>
+
+  abstract count(): Promise<number>
 }
