38cd2476f25480bff6ba22154a0987ca6980833b
JPDovale <joaopaulodovaledeo.60@gmail.com>
2024-01-10 17:26:33 -0300

    [add] list sellers on seller module

--- src/modules/seller/seller.module.ts
+++ src/modules/seller/seller.module.ts
@@ -7,10 +7,16 @@ import { LoginSellerService } from './services/loginSeller.service'
 import { LoginSellerController } from './controllers/loginSeller.controller'
 import { GetSellerController } from './controllers/getSeller.controller'
 import { GetSellerService } from './services/getSeller.service'
+import { ListSellersController } from './controllers/listSellers.controller'
+import { ListSellersService } from './services/listSellers.service'
 
 @Module({
-  controllers: [LoginSellerController, GetSellerController],
+  controllers: [
+    LoginSellerController,
+    GetSellerController,
+    ListSellersController,
+  ],
   imports: [DatabaseModule, CryptographyModule, EnvModule, DateModule],
-  providers: [LoginSellerService, GetSellerService],
+  providers: [LoginSellerService, GetSellerService, ListSellersService],
 })
 export class SellerModule {}
