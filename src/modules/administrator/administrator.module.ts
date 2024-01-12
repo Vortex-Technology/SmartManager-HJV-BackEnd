c4e42ed42b9670dbaf7234c960e240f364e4f5a4
JPDovale <joaopaulodovaledeo.60@gmail.com>
2024-01-12 17:55:54 -0300

    [delete] create administrator

--- src/modules/administrator/administrator.module.ts
+++ src/modules/administrator/administrator.module.ts
@@ -1,8 +1,6 @@
 import { Module } from '@nestjs/common'
-import { CreateAdministratorService } from './services/createAdministrator.service'
 import { DatabaseModule } from '@infra/database/database.module'
 import { CryptographyModule } from '@providers/cryptography/cryptography.module'
-import { CreateAdministratorController } from './controllers/createAdministrator.controller'
 import { LoginAdministratorService } from './services/loginAdministrator.service'
 import { EnvModule } from '@infra/env/env.module'
 import { DateModule } from '@providers/date/date.module'
@@ -20,7 +18,6 @@ import { ListAdministratorsService } from './services/listAdministrators.service
 
 @Module({
   controllers: [
-    CreateAdministratorController,
     LoginAdministratorController,
     GetAdministratorController,
     DeleteAdministratorController,
@@ -30,7 +27,6 @@ import { ListAdministratorsService } from './services/listAdministrators.service
   ],
   imports: [DatabaseModule, CryptographyModule, DateModule, EnvModule],
   providers: [
-    CreateAdministratorService,
     LoginAdministratorService,
     GetAdministratorService,
     DeleteAdministratorService,
