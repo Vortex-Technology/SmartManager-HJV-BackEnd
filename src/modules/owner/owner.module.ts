878b2a8552d1a6e505ce11b9168cdeb0da0c11e3
JPDovale <joaopaulodovaledeo.60@gmail.com>
2024-01-13 15:10:29 -0300

    [delete] login owner

--- src/modules/owner/owner.module.ts
+++ src/modules/owner/owner.module.ts
@@ -5,18 +5,12 @@ import { CryptographyModule } from '@providers/cryptography/cryptography.module'
 import { CreateOwnerController } from './controllers/createOwner.controller'
 import { GetOwnerController } from './controllers/getOwner.controller'
 import { GetOwnerService } from './services/getOwner.service'
-import { LoginOwnerService } from './services/loginOwner.service'
-import { LoginOwnerController } from './controllers/loginOwner.controller'
 import { DateModule } from '@providers/date/date.module'
 import { EnvModule } from '@infra/env/env.module'
 
 @Module({
-  controllers: [
-    CreateOwnerController,
-    GetOwnerController,
-    LoginOwnerController,
-  ],
-  providers: [CreateOwnerService, GetOwnerService, LoginOwnerService],
+  controllers: [CreateOwnerController, GetOwnerController],
+  providers: [CreateOwnerService, GetOwnerService],
   imports: [DatabaseModule, CryptographyModule, DateModule, EnvModule],
 })
 export class OwnerModule {}
