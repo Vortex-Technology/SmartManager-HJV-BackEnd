96781ad76d04d3074b88692e1aa431e1613161f9
JPDovale <joaopaulodovaledeo.60@gmail.com>
2024-01-16 17:27:17 -0300

    [refactor] collaborators repository

--- src/modules/collaborator/repositories/CollaboratorsRepository.ts
+++ src/modules/collaborator/repositories/CollaboratorsRepository.ts
@@ -2,4 +2,6 @@ import { Collaborator } from '../entities/Collaborator'
 
 export abstract class CollaboratorsRepository {
   abstract findByLogin(login: string): Promise<Collaborator | null>
+  abstract createMany(collaborator: Collaborator[]): Promise<void>
+  abstract create(collaborator: Collaborator): Promise<void>
 }
