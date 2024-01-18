787b7aaf20c2ad2417329261063f2aab91b65361
JPDovale <joaopaulodovaledeo.60@gmail.com>
2024-01-18 17:09:29 -0300

    [add] implementations of handle has generator

--- test/repositories/providers/cryptography/fakeHasher.ts
+++ test/repositories/providers/cryptography/fakeHasher.ts
@@ -1,12 +1,19 @@
+import { HandleHashGenerator } from '@providers/cryptography/contracts/handleHashGenerator'
 import { HashComparer } from '@providers/cryptography/contracts/hashComparer'
 import { HashGenerator } from '@providers/cryptography/contracts/hashGenerator'
 
-export class FakeHasher implements HashGenerator, HashComparer {
+export class FakeHasher
+  implements HashGenerator, HashComparer, HandleHashGenerator
+{
   async hash(plain: string): Promise<string> {
     return plain.concat('-hashed')
   }
 
   async compare(plain: string, hash: string): Promise<boolean> {
     return plain.concat('-hashed') === hash
+  }
+
+  async handleHash(): Promise<string> {
+    return `${Math.random()}-handle-hashed`
   }
 }
