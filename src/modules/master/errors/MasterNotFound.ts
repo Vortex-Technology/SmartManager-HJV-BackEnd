1dbbfab10c0863a4254506716243175009bba76a
JPDovale <joaopaulodovaledeo.60@gmail.com>
2024-01-18 16:50:52 -0300

    [delete] master

--- src/modules/master/errors/MasterNotFound.ts
+++
@@ -1,9 +0,0 @@
-import { ServiceError } from '@shared/core/error/ServiceError'
-
-export class MasterNotFound extends Error implements ServiceError {
-  status = 400
-
-  constructor() {
-    super('Master not found')
-  }
-}
--- src/modules/master/repositories/MastersRepository.ts
+++
@@ -1,5 +0,0 @@
-import { Master } from '../entities/Master'
-
-export abstract class MastersRepository {
-  abstract findById(id: string): Promise<Master | null>
-}
