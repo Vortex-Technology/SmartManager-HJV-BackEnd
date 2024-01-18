57216ddef5fda05e70f5572b2a04b2a3a89973db
JPDovale <joaopaulodovaledeo.60@gmail.com>
2024-01-18 16:54:40 -0300

    [refactor] collaborators repositories

--- src/infra/database/prisma/collaborator/CollaboratorsPrismaMapper.ts
+++ src/infra/database/prisma/collaborator/CollaboratorsPrismaMapper.ts
@@ -2,22 +2,47 @@ import {
   Collaborator,
   CollaboratorRole,
 } from '@modules/collaborator/entities/Collaborator'
-import { Collaborator as CollaboratorPrisma } from '@prisma/client'
+import {
+  Collaborator as CollaboratorPrisma,
+  Prisma,
+  RoleCollaborator,
+} from '@prisma/client'
 import { UniqueEntityId } from '@shared/core/entities/valueObjects/UniqueEntityId'
 
 export class CollaboratorsPrismaMapper {
   static toEntity(raw: CollaboratorPrisma): Collaborator {
-    return Collaborator.create(
+    return Collaborator.createUntyped(
       {
-        login: raw.login,
-        name: raw.name,
+        email: raw.email,
         password: raw.password,
         createdAt: raw.createdAt,
         deletedAt: raw.deletedAt,
-        image: raw.image,
+        actualRemuneration: raw.actualRemuneration,
+        marketId: new UniqueEntityId(raw.marketId),
+        userId: new UniqueEntityId(raw.userId),
+        inactivatedAt: raw.inactivatedAt,
+        updatedAt: raw.updatedAt,
         role: raw.role as CollaboratorRole,
       },
       new UniqueEntityId(raw.id),
     )
+  }
+
+  static toPrisma(
+    collaborator: Collaborator,
+  ): Prisma.CollaboratorUncheckedCreateInput {
+    return {
+      id: collaborator.id.toString(),
+      email: collaborator.email,
+      password: collaborator.password,
+      createdAt: collaborator.createdAt,
+      deletedAt: collaborator.deletedAt,
+      actualRemuneration: collaborator.actualRemuneration,
+      marketId: collaborator.marketId.toString(),
+      userId: collaborator.userId.toString(),
+      inactivatedAt: collaborator.inactivatedAt,
+      updatedAt: collaborator.updatedAt,
+      role: collaborator.role as RoleCollaborator,
+    }
   }
 }
--- test/repositories/modules/collaborator/CollaboratorsInMemoryRepository.ts
+++ test/repositories/modules/collaborator/CollaboratorsInMemoryRepository.ts
@@ -2,16 +2,19 @@ import {
   Collaborator,
   CollaboratorRole,
 } from '@modules/collaborator/entities/Collaborator'
-import { CollaboratorsRepository } from '@modules/collaborator/repositories/CollaboratorsRepository'
+import {
+  CollaboratorsRepository,
+  FindManyByMarketIdProps,
+} from '@modules/collaborator/repositories/CollaboratorsRepository'
 
 export class CollaboratorsInMemoryRepository
   implements CollaboratorsRepository
 {
   collaborators: Collaborator[] = []
 
-  async findByLogin(login: string): Promise<Collaborator | null> {
+  async findByEmail(email: string): Promise<Collaborator | null> {
     const collaborator = this.collaborators.find(
-      (collaborator) => collaborator.login === login,
+      (collaborator) => collaborator.email === email,
     )
 
     if (!collaborator) {
@@ -25,5 +28,43 @@ export class CollaboratorsInMemoryRepository
     collaborator: Collaborator<CollaboratorRole>[],
   ): Promise<void> {
     this.collaborators.push(...collaborator)
+  }
+
+  async findById(id: string): Promise<Collaborator<CollaboratorRole> | null> {
+    const collaborator = this.collaborators.find(
+      (collaborator) => collaborator.id.toString() === id,
+    )
+
+    if (!collaborator) return null
+
+    return collaborator
+  }
+
+  async create(collaborator: Collaborator<CollaboratorRole>): Promise<void> {
+    this.collaborators.push(collaborator)
+  }
+
+  async findManyByMarketId({
+    marketId,
+    page,
+    limit,
+  }: FindManyByMarketIdProps): Promise<Collaborator<CollaboratorRole>[]> {
+    const collaboratorsOfMarket = this.collaborators.filter(
+      (collaborator) => collaborator.marketId.toString() === marketId,
+    )
+
+    const collaboratorsInOrd = collaboratorsOfMarket.sort(
+      (a, b) => a.createdAt.getTime() - b.createdAt.getTime(),
+    )
+
+    return collaboratorsInOrd.slice((page - 1) * limit, page * limit)
+  }
+
+  async countByMarketId(marketId: string): Promise<number> {
+    const collaboratorsOfMarket = this.collaborators.filter(
+      (collaborator) => collaborator.marketId.toString() === marketId,
+    )
+
+    return collaboratorsOfMarket.length
   }
 }
