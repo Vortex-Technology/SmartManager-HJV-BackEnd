fca33565d7200553fb3a973fd794d3c4488ad51d
JPDovale <joaopaulodovaledeo.60@gmail.com>
2024-01-18 16:49:43 -0300

    [delete] administrator

--- src/modules/administrator/errors/AdministratorNotFound.ts
+++
@@ -1,9 +0,0 @@
-import { ServiceError } from '@shared/core/error/ServiceError'
-
-export class AdministratorNotFount extends Error implements ServiceError {
-  status = 400
-
-  constructor() {
-    super('Administrator not found')
-  }
-}
