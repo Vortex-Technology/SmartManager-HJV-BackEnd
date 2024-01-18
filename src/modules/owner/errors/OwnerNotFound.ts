fb952549132501ad61feb78de3448020facf2e75
JPDovale <joaopaulodovaledeo.60@gmail.com>
2024-01-18 16:51:48 -0300

    [delete] owner

--- src/modules/owner/errors/OwnerNotFound.ts
+++
@@ -1,9 +0,0 @@
-import { ServiceError } from '@shared/core/error/ServiceError'
-
-export class OwnerNotFount extends Error implements ServiceError {
-  status = 400
-
-  constructor() {
-    super('Owner not found')
-  }
-}
--- src/modules/owner/repositories/OwnersRepository.ts
+++
@@ -1,7 +0,0 @@
-import { Owner } from '../entities/Owner'
-
-export abstract class OwnersRepository {
-  abstract findByLogin(login: string): Promise<Owner | null>
-  abstract findById(id: string): Promise<Owner | null>
-  abstract create(owner: Owner): Promise<void>
-}
