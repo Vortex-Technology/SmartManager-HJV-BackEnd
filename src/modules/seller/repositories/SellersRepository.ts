2b5e8e09569f5707df8ceaa5b21ad8c383a41104
JPDovale <joaopaulodovaledeo.60@gmail.com>
2024-01-18 16:57:40 -0300

    [refactor] rename sellers repositories

--- src/modules/seller/repositories/SellersRepository.ts
+++ src/modules/seller/repositories/SellersRepository.ts
@@ -1,8 +1,7 @@
 import { Seller } from '../entities/Seller'
 
-export abstract class SellerRepository {
+export abstract class SellersRepository {
   abstract create(seller: Seller): Promise<void>
-  abstract findByLogin(login: string): Promise<Seller | null>
   abstract findById(id: string): Promise<Seller | null>
   abstract findMany(props: { page: number; limit: number }): Promise<Seller[]>
   abstract count(): Promise<number>
--- test/repositories/modules/seller/SellersInMemoryRepository.ts
+++ test/repositories/modules/seller/SellersInMemoryRepository.ts
@@ -1,19 +1,11 @@
 import { Seller } from '@modules/seller/entities/Seller'
-import { SellerRepository } from '@modules/seller/repositories/SellerRepository'
+import { SellersRepository } from '@modules/seller/repositories/SellersRepository'
 
-export class SellerInMemoryRepository implements SellerRepository {
+export class SellersInMemoryRepository implements SellersRepository {
   sellers: Seller[] = []
 
   async create(seller: Seller): Promise<void> {
     this.sellers.push(seller)
-  }
-
-  async findByLogin(login: string): Promise<Seller | null> {
-    const seller = this.sellers.find((seller) => seller.login === login)
-
-    if (!seller) return null
-
-    return seller
   }
 
   async findById(id: string): Promise<Seller | null> {
