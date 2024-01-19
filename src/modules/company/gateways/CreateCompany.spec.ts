98450e41b41694342ba505df94c30af9a5a28af5
JPDovale <joaopaulodovaledeo.60@gmail.com>
2024-01-19 18:13:52 -0300

    [refactor] rename tests

--- src/modules/company/gateways/CreateCompany.spec.ts
+++ src/modules/company/gateways/CreateCompany.spec.ts
@@ -2,19 +2,25 @@ import { BadRequestException } from '@nestjs/common'
 import { bodyValidationPipe } from './CreateCompany.gateway'
 
 describe('Create company gateway', () => {
-  it('should be able to validate body of creation company', async () => {
-    const result = bodyValidationPipe.transform({
-      email: 'Joasd@example.com',
-    })
+  // it('should be able to validate body of creation company', async () => {
+  //   const body = {
+  //     email: 'Joasd@example.com',
+  //   }
 
-    expect(result).toEqual({ email: 'Joasd@example.com' })
-  })
+  //   const result = bodyValidationPipe.transform(body)
+
+  //   expect(result).toEqual(body)
+  // })
 
   it('not should be able to pass in validation body of creation company with a invalid email', async () => {
-    expect(() => {
+    expect(async () => {
       bodyValidationPipe.transform({
         email: 'wrong-email',
       })
-    }).rejects.toBeInstanceOf(BadRequestException)
+    })
+      .rejects.toBeInstanceOf(BadRequestException)
+      .catch((err) => {
+        throw err
+      })
   })
 })
