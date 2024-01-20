ad6595392d47742226100a1958b0516b2a38ccd1
Stormlig <114263838+Stormlig@users.noreply.github.com>
2024-01-20 19:02:26 -0300

    [add] gateway createProduct

---
+++ src/modules/product/gateways/createProduct.gateway.ts
@@ -0,0 +1,36 @@
+import { ProductUnitType } from '../entities/Product'
+import { z } from 'zod'
+import { ZodValidationPipe } from '@shared/pipes/zodValidation'
+
+const createProductBodySchema = z.object({
+  name: z.string().min(3).max(60),
+  categories: z.array(z.string()).optional(),
+  inventoryId: z.string().uuid().optional(),
+  variants: z
+    .array(
+      z.object({
+        name: z.string().min(3).max(60),
+        description: z.string().max(190).optional(),
+        model: z.string().max(60).optional(),
+        pricePerUnit: z.number().min(5),
+        brand: z.string().min(2).max(60),
+        image: z.string().url().optional(),
+        barCode: z.string().max(48),
+        quantity: z.coerce.number().min(1),
+        unitType: z.enum([
+          ProductUnitType.UNIT,
+          ProductUnitType.KILOS,
+          ProductUnitType.CENTIMETERS,
+          ProductUnitType.LITERS,
+          ProductUnitType.METERS,
+          ProductUnitType.POL,
+        ]),
+      }),
+    )
+    .min(1),
+})
+
+export type CreateProductBody = z.infer<typeof createProductBodySchema>
+export const createProductBodyValidationPipe = new ZodValidationPipe(
+  createProductBodySchema,
+)
