b3a240c138a2ad065e33e4c76d566cf716961d02
JPDovale <joaopaulodovaledeo.60@gmail.com>
2024-01-23 20:59:25 -0300

    [add] create market on market module

--- src/modules/market/Market.module.ts
+++ src/modules/market/Market.module.ts
@@ -1,8 +1,13 @@
 import { DatabaseModule } from '@infra/database/Database.module'
 import { Module } from '@nestjs/common'
 import { CryptographyModule } from '@providers/cryptography/Cryptography.module'
+import { CreateMarketService } from './services/CreateMarket.service'
+import { CreateMarketController } from './controllers/CreateMarket.controller'
+import { ValidateApiKeyService } from '@modules/company/services/ValidateApiKey.service'
 
 @Module({
   imports: [DatabaseModule, CryptographyModule],
+  providers: [CreateMarketService, ValidateApiKeyService],
+  controllers: [CreateMarketController],
 })
 export class MarketModule {}
