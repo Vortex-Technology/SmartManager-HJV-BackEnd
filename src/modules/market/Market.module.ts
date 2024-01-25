b65179b5b8bcd8b19f09b3874d852aeada71d94f
JPDovale <joaopaulodovaledeo.60@gmail.com>
2024-01-25 13:56:53 -0300

    [add] add collaborator market on market module

--- src/modules/market/Market.module.ts
+++ src/modules/market/Market.module.ts
@@ -4,10 +4,16 @@ import { CryptographyModule } from '@providers/cryptography/Cryptography.module'
 import { CreateMarketService } from './services/CreateMarket.service'
 import { CreateMarketController } from './controllers/CreateMarket.controller'
 import { ValidateApiKeyService } from '@modules/company/services/ValidateApiKey.service'
+import { AddCollaboratorMarketController } from './controllers/AddCollaboratorMarket.controller'
+import { AddCollaboratorMarketService } from './services/AddCollaboratorMarket.service'
 
 @Module({
   imports: [DatabaseModule, CryptographyModule],
-  providers: [CreateMarketService, ValidateApiKeyService],
-  controllers: [CreateMarketController],
+  providers: [
+    CreateMarketService,
+    ValidateApiKeyService,
+    AddCollaboratorMarketService,
+  ],
+  controllers: [CreateMarketController, AddCollaboratorMarketController],
 })
 export class MarketModule {}
