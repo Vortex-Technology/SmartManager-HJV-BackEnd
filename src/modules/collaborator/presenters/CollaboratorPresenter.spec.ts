b03e1a8355d05a2bdeb51ae7b0d50c87e6c963c0
JPDovale <joaopaulodovaledeo.60@gmail.com>
2024-01-27 18:46:51 -0300

    [add] collaborator presenter

---
+++ src/modules/collaborator/presenters/CollaboratorPresenter.spec.ts
@@ -0,0 +1,15 @@
+import { CollaboratorPresenter } from './CollaboratorPresenter'
+
+let sut: CollaboratorPresenter
+
+describe('CollaboratorPresenter', async () => {
+  beforeEach(async () => {
+    sut = new CollaboratorPresenter()
+  })
+
+  it.skip('should ...', async () => {
+    // Arrange
+    // Act
+    // Assert
+  })
+})
