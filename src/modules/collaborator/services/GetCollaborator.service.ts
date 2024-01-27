51e9d24946ecc0e735ed19541a543c0de7ed5e47
JPDovale <joaopaulodovaledeo.60@gmail.com>
2024-01-27 18:41:48 -0300

    [refactor] get collaborator service to use verifications of permission

--- src/modules/collaborator/services/GetCollaborator.service.ts
+++ src/modules/collaborator/services/GetCollaborator.service.ts
@@ -1,27 +1,49 @@
 import { Injectable } from '@nestjs/common'
 import { Either, left, right } from '@shared/core/error/Either'
-import { Collaborator } from '../entities/Collaborator'
-import { CollaboratorsRepository } from '../repositories/CollaboratorsRepository'
+import { Collaborator, CollaboratorRole } from '../entities/Collaborator'
 import { CollaboratorNotFound } from '../errors/CollaboratorNotFound'
+import { PermissionDenied } from '@shared/errors/PermissionDenied'
+import { VerifyPermissionsOfCollaboratorInMarketService } from '@modules/interceptors/services/VerifyPermissionsOfCollaboratorInMarket.service'
+import { MarketNotFound } from '@modules/market/errors/MarketNorFound'
+import { CompanyNotFound } from '@modules/company/errors/CompanyNotFound'
 
 interface Request {
-  id: string
+  collaboratorId: string
+  companyId: string
+  marketId: string
 }
 
-type Response = Either<CollaboratorNotFound, { collaborator: Collaborator }>
+type Response = Either<
+  CollaboratorNotFound | PermissionDenied | MarketNotFound | CompanyNotFound,
+  { collaborator: Collaborator }
+>
 
 @Injectable()
 export class GetCollaboratorService {
   constructor(
-    private readonly collaboratorsRepository: CollaboratorsRepository,
+    private readonly verifyPermissions: VerifyPermissionsOfCollaboratorInMarketService,
   ) {}
 
-  async execute({ id }: Request): Promise<Response> {
-    const collaborator = await this.collaboratorsRepository.findById(id)
+  async execute({
+    collaboratorId,
+    companyId,
+    marketId,
+  }: Request): Promise<Response> {
+    const response = await this.verifyPermissions.execute({
+      acceptedRoles: [
+        CollaboratorRole.MANAGER,
+        CollaboratorRole.OWNER,
+        CollaboratorRole.SELLER,
+        CollaboratorRole.STOCKIST,
+      ],
+      collaboratorId,
+      companyId,
+      marketId,
+    })
 
-    if (!collaborator) {
-      return left(new CollaboratorNotFound())
-    }
+    if (response.isLeft()) return left(response.value)
+
+    const { collaborator } = response.value
 
     return right({ collaborator })
   }
