3a519497ad0559caa9d56b93b34588c5487504cd
JPDovale <joaopaulodovaledeo.60@gmail.com>
2024-01-27 23:56:15 -0300

    [add] create order service

---
+++ src/modules/order/services/CreateOrder.service.spec.ts
@@ -0,0 +1,15 @@
+import { CreateOrderService } from './CreateOrder.service'
+
+let sut: CreateOrderService
+
+describe('CreateOrderService', async () => {
+  beforeEach(async () => {
+    sut = new CreateOrderService()
+  })
+
+  it.skip('should ...', async () => {
+    // Arrange
+    // Act
+    // Assert
+  })
+})
