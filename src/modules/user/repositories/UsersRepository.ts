62f21a72df5c533e9fe245c883dce1397b6bd51d
Luan Victor <lvictor.dutra@gmail.com>
2024-01-29 19:13:01 -0300

    [feat] add update abstract  in repository

--- src/modules/user/repositories/UsersRepository.ts
+++ src/modules/user/repositories/UsersRepository.ts
@@ -3,5 +3,6 @@ import { User } from '../entities/User'
 export abstract class UsersRepository {
   abstract findByEmail(email: string): Promise<User | null>
   abstract create(user: User): Promise<void>
+  abstract save(user: User): Promise<void>
   abstract findById(id: string): Promise<User | null>
 }
