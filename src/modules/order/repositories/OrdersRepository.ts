e3b1da6fc2504373318b35c7bdbf93320f7b2a9c
JPDovale <joaopaulodovaledeo.60@gmail.com>
2024-01-30 15:13:01 -0300

    [add] methods on orders repository

--- src/modules/order/repositories/OrdersRepository.ts
+++ src/modules/order/repositories/OrdersRepository.ts
@@ -1,5 +1,7 @@
 import { Order } from '../entities/Order'
 
-export abstract class OrdersRepository {
+export abstract class OrdersRepository<ConfigT = unknown> {
   abstract create(order: Order): Promise<void>
+  abstract findByIdWithProducts(id: string): Promise<Order | null>
+  abstract save(order: Order, config?: ConfigT): Promise<void>
 }
