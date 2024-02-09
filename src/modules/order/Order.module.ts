3695ee6a0ecff2f451f452c2b5fa6b8ea19d8b4b
JPDovale <joaopaulodovaledeo.60@gmail.com>
2024-02-09 19:46:59 -0300

    [add] close order on order  module

--- src/modules/order/Order.module.ts
+++ src/modules/order/Order.module.ts
@@ -7,14 +7,22 @@ import { CreateOrderController } from './controllers/CreateOrder.controller'
 import { CreateOrderService } from './services/CreateOrder.service'
 import { AddProductOnOrderService } from './services/AddProductOnOrder.service'
 import { AddProductOnOrderController } from './controllers/AddProductOnOrder.controller'
+import { CloseOrderController } from './controllers/CloseOrder.controller'
+import { CloseOrderService } from './services/CloseOrder.service'
+import { DocModule } from '@providers/docs/Doc.module'
 
 @Module({
-  imports: [DatabaseModule, InterceptorsModule, CryptographyModule],
-  controllers: [CreateOrderController, AddProductOnOrderController],
+  imports: [DatabaseModule, InterceptorsModule, CryptographyModule, DocModule],
+  controllers: [
+    CreateOrderController,
+    AddProductOnOrderController,
+    CloseOrderController,
+  ],
   providers: [
     ValidateApiKeyService,
     CreateOrderService,
     AddProductOnOrderService,
+    CloseOrderService,
   ],
 })
-export class OrderModule {}
+export class OrderModule { }
